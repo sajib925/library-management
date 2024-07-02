fad202ce7c71fceee44acdbd5eef4c372d0848ad
sajib925 <sajibahmed1004@gmail.com>
2024-07-03 00:57:59 +0600

    database added

--- library_management/settings.py
+++ library_management/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,14 +83,21 @@ WSGI_APPLICATION = 'library_management.wsgi.application'
 
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://library_4bqu_user:YApyclPovVOAkgcKn6Exwp668BMSOnHF@dpg-cq24qqdumphs73e92rl0-a.oregon-postgres.render.com/library_4bqu',
+    )
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
